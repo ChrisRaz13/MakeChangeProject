bb4dae3119dfd5afad51b41ffacaf53d0c23b4c8
Christopher Rasmussen <Chris.E.Rasmussen1@gmail.com>
2023-11-19 21:09:55 +0100

    essages

--- src/makeChange/CashRegister.java
+++ src/makeChange/CashRegister.java
@@ -1,9 +1,41 @@
 package makeChange;
 
+import java.util.Scanner;
+
 public class CashRegister {
 
 	public static void main(String[] args) {
 //		 Testing git push
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("What is the price of the item?");
+		double price = sc.nextDouble();
+		System.out.println("The item costs $" + price);
+		
+		System.out.println("How much money was tendered by the customer?");
+		double paid = sc.nextDouble();
+		System.out.println("Customer paid $" + paid);
+		
+		if (price == paid) {
+			System.out.println("Exact amount given");
+		}	
+		else if (paid < price) {
+			System.out.println("Did not pay enough");
+		}
+		else if (paid > price) {
+			
+		}
+		
+	
+			
+		
+		
+		sc.close();
+		
+		
+		
 	}
 
 }
